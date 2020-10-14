0f94f0ca115a65e9b91e170ef3a117e80fde6b5a
Erhan Gungor <erhangungor@zohomail.com>
2020-10-14 13:16:01 +0300

    initial commit

---
+++ backend/src/main/java/com/erhan/InventoryManagementWebApp/InventoryManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.erhan.InventoryManagementWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/erhan/InventoryManagementWebApp/InventoryManagementWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erhan.InventoryManagementWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
